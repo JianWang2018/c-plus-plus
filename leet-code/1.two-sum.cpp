6c89de25b5f42f522f1ce5ab6e6af382f8ffb297
wangjian790 <wangjian790@gmail.com>
2016-12-22 14:22:37 -0500

    revision

---
+++ leet-code/1.two-sum.cpp
@@ -0,0 +1,23 @@
+# include <iostream>
+# include <leet-code.h>
+#include <vector>
+
+/*
+Problem:Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution.
+
+Example:
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+
+using namespace std;*/
+
+vector<int>  Solution::twoSum(vector<int> &nums, int target)
+{
+    cout<<"hello_world"<<endl;
+    vector<int> a;
+    return a; 
+}
