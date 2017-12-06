b4642a198bdf583dc4025ac76a638d40b9b250f0
wangjian790 <wangjian790@gmail.com>
2017-12-06 12:01:59 -0500

    init

---
+++ option_pricing/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
+CND_ARTIFACT_NAME_Debug=option_pricing
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/option_pricing
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
+CND_PACKAGE_NAME_Debug=optionpricing.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/optionpricing.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
+CND_ARTIFACT_NAME_Release=option_pricing
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/option_pricing
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
+CND_PACKAGE_NAME_Release=optionpricing.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/optionpricing.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
